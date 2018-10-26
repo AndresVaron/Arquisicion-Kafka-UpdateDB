a6cb07ef4e1e5a0b7bf137c8707f0f71228f3f55
AndresVaron <af.varon@uniandes.edu.co>
2018-10-25 21:56:42 -0500

    arreglos

--- Pagos.py
+++ Pagos.py
@@ -5,17 +5,20 @@ from kafka.errors import KafkaError
 from random import uniform
 consumer = KafkaConsumer(bootstrap_servers=['ec2-35-161-3-13.us-west-2.compute.amazonaws.com:8081'],
 	value_deserializer=lambda m: json.loads(m.decode('utf-8')))
-consumer.subscribe(topics=("Update_Cupos_v2"))
+consumer.subscribe(topics=("PAGOS"))
 conn = None
 try:
     conn = psycopg2.connect(host="ec2-54-185-19-120.us-west-2.compute.amazonaws.com",database="arquisicion", user="admin", password="ARQUISICION2018",port = "5432")
-    print("Iniciado servicio de atualizacion de cupos")
+    print("Iniciado servicio de pagos")
     for message in consumer:
-        print("id = %s  || cupos = %s" % (message.value['id'], message.value['cupos']))
-        cur = conn.cursor()
-        cur.execute("UPDATE parqueaderoempresaentity SET cupos = " + str(message.value['cupos']) + " WHERE id = " + str(message.value['id'])+ ";")
-        conn.commit()
-        cur.close()
+        print("id = %s  || tipo = %s" % (message.value['id'], message.value['tipo']))
+        if message.value['tipo'] == 'OnDemand':
+            cur = conn.cursor()
+            print("Pagando...")
+            cur.execute("UPDATE  contratoondemandentity SET estado = 0 WHERE id = " + str(message.value['id'])+ ";")
+            conn.commit()
+            cur.close()
+            print("Pago realizado")
 except (Exception, psycopg2.DatabaseError) as error:
     print(error)
 finally:
--- UpdateConexion.py
+++ UpdateConexion.py
@@ -4,12 +4,12 @@ from kafka import KafkaConsumer, KafkaProducer
 from kafka.errors import KafkaError
 from random import uniform
 
-consumer = KafkaConsumer(bootstrap_servers=['172.24.41.199:8084'],
+consumer = KafkaConsumer(bootstrap_servers=['ec2-35-161-3-13.us-west-2.compute.amazonaws.com:8081'],
 	value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 consumer.subscribe(topics=("Update_Conexion"))
 conn = None
 try:
-    conn = psycopg2.connect(host="172.24.99.21",database="arquisicion", user="admin", password="admin123",port = "5432")
+    conn = psycopg2.connect(host="ec2-54-185-19-120.us-west-2.compute.amazonaws.com",database="arquisicion", user="admin", password="ARQUISICION2018",port = "5432")
     print("Iniciado servicio de atualizacion de Conexiones")
     for message in consumer:
         print("id = %s || ip = %s || puerto = %s"  % (message.value['id'], message.value['ip'], message.value['puerto']))
