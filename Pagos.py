50fd6dbee075fe2c5b23b7f3b877f9493f22bf2b
AndresVaron <35813015+AndresVaron@users.noreply.github.com>
2021-01-07 08:30:24 -0500

    Update Pagos.py

--- Pagos.py
+++ Pagos.py
@@ -8,7 +8,7 @@ consumer = KafkaConsumer(bootstrap_servers=['ec2-35-161-3-13.us-west-2.compute.a
 consumer.subscribe(topics=("PAGOS"))
 conn = None
 try:
-    conn = psycopg2.connect(host="ec2-54-185-19-120.us-west-2.compute.amazonaws.com",database="arquisicion", user="admin", password="ARQUISICION2018",port = "5432")
+    conn = psycopg2.connect()
     print("Iniciado servicio de pagos")
     for message in consumer:
         print("id = %s  || tipo = %s" % (message.value['id'], message.value['tipo']))
